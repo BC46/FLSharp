434334ba1c97529c5c3b7cd131120bef51d35a98
C.S. Melis <cees3331x00@gmail.com>
2024-01-27 18:13:17 +0100

    Successfully hook CheckForSync

--- include/Common.h
+++ include/Common.h
@@ -9,3 +9,20 @@ struct Import CShip
 {
     float get_throttle() const;
 };
+
+class Vector
+{
+public:
+    float x, y, z;
+};
+
+class Quaternion
+{
+public:
+    float w, x, y, z;
+};
+
+struct Import CRemotePhysicsSimulation
+{
+    bool CheckForSync(Vector const &unk1, Vector const &unk2, Quaternion const &unk3);
+};
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,9 @@
 #include "RemoteServer.h"
 #include "utils.h"
 
+#define CHECK_FOR_SYNC_CALL_ADDR 0x541602
 #define OBJ_UPDATE_CALL_ADDR 0x54167C
+
 #define Naked __declspec(naked)
 
 const DWORD Get_IOBjRW_ADDR = 0x54BAF0;
@@ -21,6 +23,20 @@ Naked CShip* GetCShip()
     }
 }
 
+// Determines whether an update should be sent to the server
+bool __fastcall CheckForSync_Hook(CRemotePhysicsSimulation* physicsSim, PVOID _edx, Vector const &unk1, Vector const &unk2, Quaternion const &unk3)
+{
+    // Call the original function and sync if it returns true
+    if (physicsSim->CheckForSync(unk1, unk2, unk3))
+        return true;
+
+    // If the original function doesn't want to sync, we do our own checks to see if it should sync regardless
+    // TODO: do checks here
+
+    return false;
+}
+
+// Function that sends an update to the server
 void __fastcall SPObjUpdate_Hook(IServerImpl* server, PVOID _edx, SSPObjUpdateInfo &updateInfo, UINT client)
 {
     CShip* ship = GetCShip();
@@ -35,6 +51,9 @@ void __fastcall SPObjUpdate_Hook(IServerImpl* server, PVOID _edx, SSPObjUpdateIn
 
 void Init()
 {
+    // Hook CheckForSync function call
+    Hook(CHECK_FOR_SYNC_CALL_ADDR, (DWORD) CheckForSync_Hook, 5);
+
     // Hook SPObjUpdate function call
     Hook(OBJ_UPDATE_CALL_ADDR, (DWORD) SPObjUpdate_Hook, 6);
 }
