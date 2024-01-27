b40c93926667efad2f1b31fc4b4c790632c04dd7
C.S. Melis <cees3331x00@gmail.com>
2024-01-27 17:39:07 +0100

    Add Hook and Nop methods

--- src/main.cpp
+++ src/main.cpp
@@ -20,12 +20,37 @@ Naked CShip* GetCShip()
     }
 }
 
-DWORD _;
-
 void Patch(LPVOID vOffset, LPVOID mem, UINT len)
 {
+    static DWORD _;
+
     VirtualProtect(vOffset, len, PAGE_EXECUTE_READWRITE, &_);
     memcpy(vOffset, mem, len);
+}
+
+void Nop(LPVOID vOffset, UINT len)
+{
+    if (len == 0)
+        return;
+
+    static DWORD _;
+
+    VirtualProtect(vOffset, len, PAGE_EXECUTE_READWRITE, &_);
+    memset(vOffset, 0x90, len);
+}
+
+void Hook(DWORD location, DWORD hookFunc, UINT instrLen)
+{
+    // Set the opcode for the call instruction
+    static BYTE callOpcode = 0xE8;
+    Patch((PVOID) location, &callOpcode, sizeof(BYTE));
+
+    // Set and calculate the relative offset for the hook function
+    DWORD relOffset = hookFunc - location - 5;
+    Patch((PVOID) (location + 1), &relOffset, sizeof(DWORD));
+
+    // Nop out excess bytes
+    Nop((PVOID) (location + 5), instrLen - 5);
 }
 
 void __fastcall SPObjUpdate_Hook(IServerImpl* server, PVOID _edx, SSPObjUpdateInfo &updateInfo, UINT client)
@@ -42,14 +67,8 @@ void __fastcall SPObjUpdate_Hook(IServerImpl* server, PVOID _edx, SSPObjUpdateIn
 
 void Init()
 {
-    BYTE callAbsOpcode = 0x15; // Second byte of the absolute dword ptr call opcode
-    Patch((PVOID) (OBJ_UPDATE_CALL_ADDR + 1), &callAbsOpcode, sizeof(BYTE)); // Change opcode such that we can hook the function call
-
-    static DWORD hookPtr = (DWORD) SPObjUpdate_Hook;
-    DWORD hookPtrRef = (DWORD) &hookPtr;
-
     // Hook SPObjUpdate function call
-    Patch((PVOID) (OBJ_UPDATE_CALL_ADDR + 2), &hookPtrRef, sizeof(DWORD));
+    Hook(OBJ_UPDATE_CALL_ADDR, (DWORD) SPObjUpdate_Hook, 6);
 }
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
@@ -58,9 +77,7 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
     UNREFERENCED_PARAMETER(lpReserved);
 
     if (fdwReason == DLL_PROCESS_ATTACH)
-    {
         Init();
-    }
 
     return TRUE;
 }
