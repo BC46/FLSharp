a925ed039f45b7113bf3b2cbd1bcc5966d5af23e
Cees Melis <cees3331x00@gmail.com>
2025-01-04 18:06:08 +0100

    Add MSVC version check for copysign

--- include/fl_math.h
+++ include/fl_math.h
@@ -23,7 +23,11 @@ float GetRotationDelta(const Quaternion& quat, const Matrix& rot);
 
 Quaternion MatrixToQuaternion(const Matrix& m);
 
+#ifdef _MSC_VER
+#if _MSC_VER < 1700
 float inline copysign(float x, float y)
 {
     return (x < 0 && y > 0) || (x > 0 && y < 0) ? -x : x;
 }
+# endif
+#endif
