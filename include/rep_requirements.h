c8b33fa1407bcdf8c14afecf69c78ec0ed96deff
Cees Melis <cees3331x00@gmail.com>
2025-02-08 20:51:36 +0100

    Store ship reputation requirements

--- include/rep_requirements.h
+++ include/rep_requirements.h
@@ -16,3 +16,18 @@ struct NN_Dealer
 private:
     typedef void (NN_Dealer::*PrintFmtStrPurchaseInfo)(UINT idsPurchaseInfo, int fmtValue);
 };
+
+#define SHIP_TRADER_SHIP_AMOUNT 3
+
+struct NN_ShipTrader
+{
+    BYTE x00[0x448];
+    int shipRepPercentages[SHIP_TRADER_SHIP_AMOUNT];
+
+    void StoreShipLevelRequirement(PBYTE shipListPtr, float repRequirement);
+};
+
+inline int GetRepPercentage(float repValue)
+{
+    return static_cast<int>(repValue * 100.0f);
+}
--- src/rep_requirements.cpp
+++ src/rep_requirements.cpp
@@ -1,14 +1,40 @@
 #include "rep_requirements.h"
 #include "utils.h"
 
+#define NAKED __declspec(naked)
+
 void NN_Dealer::PrintFmtStrPurchaseInfo_Hook(UINT idsPurchaseInfo, DealerStack* stack)
 {
-    // Get the reputation percentage from the stack (custom variable).
-    int repPercentage = static_cast<int>(stack->repRequired * 100.0f);
-
-    // Call the original function.
+    // Call the original function with the rep percentage.
     PrintFmtStrPurchaseInfo ogFunc = GetFuncDef<PrintFmtStrPurchaseInfo>(0x47FD50);
-    (this->*ogFunc)(idsPurchaseInfo, repPercentage);
+    (this->*ogFunc)(idsPurchaseInfo, GetRepPercentage(stack->repRequired));
+}
+
+NAKED void GetShipRepRequirement_Hook()
+{
+    #define STORE_SHIP_LEVEl_REQUIREMENT_RET_ADDR 0x4B9469
+
+    __asm {
+        mov [esi+0xC], eax                                  // overwritten instruction #1
+        sub esp, 0x4
+        fst dword ptr [esp]                                 // shipLevelRequirement
+        push esi                                            // shipListPtr
+        mov ecx, ebx                                        // NN_ShipTrader
+        call NN_ShipTrader::StoreShipLevelRequirement
+        mov eax, [esp+0x10]                                 // overwritten instruction #2
+        mov ecx, STORE_SHIP_LEVEl_REQUIREMENT_RET_ADDR
+        jmp ecx
+    }
+}
+
+// Calculates the ship index and stores the rep requirement as a percentage in the right location.
+void NN_ShipTrader::StoreShipLevelRequirement(PBYTE shipListPtr, float repRequirement)
+{
+    #define SHIP_LIST_PTR_START 0x3FC
+    int shipIndex = (shipListPtr - (PBYTE) this - SHIP_LIST_PTR_START) / sizeof(int);
+
+    if (shipIndex >= 0 && shipIndex < SHIP_TRADER_SHIP_AMOUNT)
+        this->shipRepPercentages[shipIndex] = GetRepPercentage(repRequirement);
 }
 
 // In FL there exists the string "You must be on friendlier terms to purchase this."
@@ -21,7 +47,15 @@ void NN_Dealer::PrintFmtStrPurchaseInfo_Hook(UINT idsPurchaseInfo, DealerStack* 
 void InitPrintRepRequirements()
 {
     #define REP_REQUIREMENTS_NOT_MET_ADDR 0x480739
+    #define NN_SHIPTRADER_OBJ_SIZE_ADDR 0x4B9739
+    #define GET_SHIP_REQUIREMENT_ADDR 0x4B9462
 
     Hook(REP_REQUIREMENTS_NOT_MET_ADDR + 0x9, &NN_Dealer::PrintFmtStrPurchaseInfo_Hook, 5);
     Patch_WORD(REP_REQUIREMENTS_NOT_MET_ADDR, 0x9054); // push esp followed by nop (replaces param 0 with a stack pointer)
+
+    // Expand the size of the NN_ShipTrader object.
+    ReadWriteProtect(NN_SHIPTRADER_OBJ_SIZE_ADDR, sizeof(UINT));
+    *(PUINT) NN_SHIPTRADER_OBJ_SIZE_ADDR += sizeof(int[SHIP_TRADER_SHIP_AMOUNT]);
+
+    Hook(GET_SHIP_REQUIREMENT_ADDR, GetShipRepRequirement_Hook, 7, true);
 }
