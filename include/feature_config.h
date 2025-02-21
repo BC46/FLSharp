89fa51b8d1d784d0e99d7389ad3504bfbfc54459
Cees Melis <cees3331x00@gmail.com>
2025-02-21 23:08:37 +0100

    Indentation and comment fixes

--- include/feature_config.h
+++ include/feature_config.h
@@ -7,30 +7,30 @@
 
 struct FlSharpFeature
 {
-	void (*initFunc)(); 		// feature's init function
-	void (*cleanupFunc)();		// feature's cleanup function
-	bool (*applyPredicate)();	// function that determines whether the feature must be applied from a technical perspective
-	bool enabled;				// value determined by the user so they can choose whether they want it to be applied
+    void (*initFunc)(); 		// feature's init function
+    void (*cleanupFunc)();		// feature's cleanup function
+    bool (*applyPredicate)();	// function that determines whether the feature must be applied from a technical perspective
+    bool enabled;				// value determined by the user so they can choose whether they want it to be applied
 };
 
 struct CmpStr
 {
-	bool operator()(LPCSTR a, LPCSTR b) const
-	{
-		return stricmp(a, b) < 0;
-	}
+    bool operator()(LPCSTR a, LPCSTR b) const
+    {
+        return stricmp(a, b) < 0;
+    }
 };
 
 class FeatureManager
 {
 public:
-	void RegisterFeature(LPCSTR name, void (*initFunc)(), void (*cleanupFunc)(), bool (*applyPredicate)());
-	void SetFeatureEnabled(LPCSTR name, bool enabled);
-	void InitFeatures();
-	void CleanupFeatures();
+    void RegisterFeature(LPCSTR name, void (*initFunc)(), void (*cleanupFunc)(), bool (*applyPredicate)());
+    void SetFeatureEnabled(LPCSTR name, bool enabled);
+    void InitFeatures();
+    void CleanupFeatures();
 
 private:
-	std::map<LPCSTR, FlSharpFeature, CmpStr> features;
+    std::map<LPCSTR, FlSharpFeature, CmpStr> features;
 };
 
 bool ApplyAlways();
