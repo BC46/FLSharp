bd54360ac01c707ff49d695e8d91e414affe7128
C.S. Melis <cees3331x00@gmail.com>
2025-07-13 19:09:26 +0200

    Document new waypoint code

--- src/waypoint.cpp
+++ src/waypoint.cpp
@@ -5,9 +5,10 @@
 #define PLAYERSHIP_NAVMAP_OBJ_TYPE 2
 #define NAV_MAP_GET_HIGHLIGHTED_OBJ_WAYPOINT_CALL_ADDR 0x493A00
 #define NAV_MAP_GET_HIGHLIGHTED_OBJ_BESTPATH_CALL_ADDR 0x493B21
-#define SIMPLE_UNVISITED_CHECK_FOR_TARGET_LIST_CALL_ADDR 0x4E4094
-#define SIMPLE_VISITED_CHECK_FOR_TARGET_LIST_CALL_ADDR 0x4755B8
+
 #define GET_UNKNOWN_SIMPLE_IDS_FOR_TARGET_LIST_CALL_ADDR 0x4E40AF
+#define SIMPLE_UNVISITED_CHECK_FOR_TARGET_LIST_CALL_ADDR 0x4E4094
+#define SIMPLE_VISITED_CHECK_FOR_CURRENT_INFO_LIST_CALL_ADDR 0x4755B8
 
 // Hook that prevents waypoints from being cleared when the player is in a different system
 Waypoint* GetWaypoint_Hook(int index)
@@ -70,13 +71,18 @@ bool IsSimpleVisited_Hook(const CSimple& simple)
 // This hook adds a space and ensures the correct number is printed.
 int swprintf_Hook(int waypointIndex)
 {
+    // FL's original code for getting the waypoint number (incorrect).
     int waypointNumber = waypointIndex + 1;
 
+    // If we can obtain the waypoint, use its waypoint number.
+    // If this approach fails, just use the value that FL would originally use.
     if (Waypoint* waypoint = GetWaypoint(waypointIndex))
     {
         waypointNumber = waypoint->waypointNumber;
     }
 
+    // Printf the waypoint number with an added space.
+    // This gives "PLAYER WAYPOINT n" instead of "PLAYER WAYPOINTn".
     return swprintf_s(FL_BUFFER_1, FL_BUFFER_LEN, L" %d\n", waypointNumber);
 }
 
@@ -85,13 +91,16 @@ UINT GetCShipOrCEqObjName_Hook(const CEqObj &eqObj)
 {
     UINT result = GetCShipOrCEqObjName(eqObj);
 
+    // Check to make sure that we're dealing with a waypoint here.
     if (result == WAYPOINT_IDS && IsObjectAWaypoint(eqObj))
     {
         int waypointIndex;
         bool isPlayerWaypoint;
 
+        // Try to check whether this is a player waypoint.
         if (WAYPOINT_WATCHER && WAYPOINT_WATCHER->GetCurrentWaypointInfo(isPlayerWaypoint, waypointIndex))
         {
+            // If it's not a player waypoint, it's a mission waypoint, so return the right IDS.
             if (!isPlayerWaypoint)
                 return MISSION_WAYPOINT_IDS;
         }
@@ -112,14 +121,20 @@ void InitWaypointFixes()
 
     // Fix waypoints being called "Unknown Object" in the target view.
     Hook(SIMPLE_UNVISITED_CHECK_FOR_TARGET_LIST_CALL_ADDR, IsSimpleUnvisited_Hook, 5); // Target selection
-    Hook(SIMPLE_VISITED_CHECK_FOR_TARGET_LIST_CALL_ADDR, IsSimpleVisited_Hook, 5); // Current Information window
+    Hook(SIMPLE_VISITED_CHECK_FOR_CURRENT_INFO_LIST_CALL_ADDR, IsSimpleVisited_Hook, 5); // Current Information window
 
-    // TODO: document
-    Patch<WORD>(0x475A6C, 0x74FF);
-    Hook(0x475A70, swprintf_Hook, 5);
-    Nop(0x475A75, 9);
-    GetValue<BYTE>(0x475A8C) -= sizeof(DWORD) * 2;
+    // Fix the player waypoint being printed incorrectly in the Current Information window.
+    #define SWPRINTF_WAYPOINT_PARAMS_ADDR 0x475A6C
+    #define WAYPOINT_INFO_PARAMS_CLEANED_STACK_ADDR 0x475A8C
+    Patch<WORD>(SWPRINTF_WAYPOINT_PARAMS_ADDR, 0x74FF); // push waypoint number onto stack
+    Hook(SWPRINTF_WAYPOINT_PARAMS_ADDR + 4, swprintf_Hook, 5);
+    Nop(SWPRINTF_WAYPOINT_PARAMS_ADDR + 4 + 5, 9); // nop out unneeded param pushes
+    // Decrease the cleaned stack by 8 bytes because we removed two params from our hook call.
+    GetValue<BYTE>(WAYPOINT_INFO_PARAMS_CLEANED_STACK_ADDR) -= sizeof(DWORD) * 2;
 
-    Hook(0x475676, GetCShipOrCEqObjName_Hook, 5);
-    Hook(0x4E8131, GetCShipOrCEqObjName_Hook, 5);
+    // Ensure player waypoints are called "Waypoint" and mission waypoints "Mission Waypoint".
+    #define GET_OBJ_NAME_CURRENT_INFO_CALL_ADDR 0x475676
+    #define GET_OBJ_NAME_TARGET_SELECTION_CALL_ADDR 0x4E8131
+    Hook(GET_OBJ_NAME_CURRENT_INFO_CALL_ADDR, GetCShipOrCEqObjName_Hook, 5); // Current Information window
+    Hook(GET_OBJ_NAME_TARGET_SELECTION_CALL_ADDR, GetCShipOrCEqObjName_Hook, 5); // Target selection
 }
