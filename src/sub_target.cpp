8b5a141e56ce8c7cc986aad58883643e46ecc565
BC46 <cees3331x00@gmail.com>
2026-07-28 18:27:25 +0200

    Refactor subtarget hotkey fix

--- src/sub_target.cpp
+++ src/sub_target.cpp
@@ -5,9 +5,11 @@
 
 #define HANDLE_SUBTARGET_HOTKEY_RET_ADDR_PREFIX 0x4E2B00
 
+// Checks if the sub-target button is visible.
+// If not, provide a return address which skips the function calls.
 DWORD Target::HandleSubtargetHotkey(bool previous)
 {
-    static const BYTE returnSuffixes[] = { 0x65, 0x86, 0x9A, 0xBB };
+    static const BYTE returnSuffixes[] = { 0x63, 0x86, 0x98, 0xBB };
 
     FlUiElement* targetButton = previous ? hudTarget.targetPreviousButton : hudTarget.targetNextButton;
     bool visible = targetButton && targetButton->IsVisible();
@@ -18,20 +20,25 @@ DWORD Target::HandleSubtargetHotkey(bool previous)
 NAKED void HandleSubtargetHotkey_Hook()
 {
     __asm {
-        lea esi, [edi-0x384]    // overwritten instruction #1
+        lea esi, [edi-0x384]    // overwritten instruction
+        push eax                // jump table index (0 for next, 1 for previous)
         mov ecx, esi            // Target*
         call Target::HandleSubtargetHotkey
-        mov ecx, esi            // overwritten instruction #2
         jmp eax
     }
 }
 
+// JFLP enables the previous and next Sub-Target hotkeys. These work fine when they are activated while the Target View is open.
+// However, if the Contact List is open instead, it will display the target and sub-target names in the Contact List.
+// This messes up the view because these names should only be displayed in the Target View.
+// The issue is fixed by ensuring the previous/next Sub-Target hotkey will only work if its respective button is visible.
 void InitSubTargetFix()
 {
-    const DWORD subtargetHotkeyHandlers[] = { 0x4E2B5D, 0x4E2B92 };
-    for (WORD i = 0; i < _countof(subtargetHotkeyHandlers); ++i)
+    #define CONTACT_LIST_HOTKEY_JMP_TABLE_ADDR 0x4E2EEC
+    #define SUBTARGET_HOTKEY_COUNT 2
+
+    for (int i = 0; i < SUBTARGET_HOTKEY_COUNT; ++i)
     {
-        Patch<WORD>(subtargetHotkeyHandlers[i], 0x006A + (i << 8));
-        Hook(subtargetHotkeyHandlers[i] + 2, HandleSubtargetHotkey_Hook, 6, true);
+        SetPointer(0x4E2EEC + i * sizeof(DWORD), HandleSubtargetHotkey_Hook);
     }
 }
